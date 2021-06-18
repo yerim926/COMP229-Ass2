9333597a7dd1ba00d0b4e7fd154ac90f2d71208d
yerim926 <yerim926@users.noreply.github.com>
2021-06-18 14:01:06 -0400

    added authguard for secure pages

--- Server/Routes/business.js
+++ Server/Routes/business.js
@@ -7,10 +7,11 @@ const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 exports.default = router;
 const business_1 = require("../Controllers/business");
+const index_1 = require("../Util/index");
 router.get('/', business_1.DisplayBusinessListPage);
-router.get('/add', business_1.DisplayAddPage);
-router.get('/edit/:id', business_1.DisplayEditPage);
-router.post('/add', business_1.ProcessAddPage);
-router.post('/edit/:id', business_1.ProcessEditPage);
-router.get('/delete/:id', business_1.ProcessDeletePage);
+router.get('/add', index_1.AuthGuard, business_1.DisplayAddPage);
+router.get('/edit/:id', index_1.AuthGuard, business_1.DisplayEditPage);
+router.post('/add', index_1.AuthGuard, business_1.ProcessAddPage);
+router.post('/edit/:id', index_1.AuthGuard, business_1.ProcessEditPage);
+router.get('/delete/:id', index_1.AuthGuard, business_1.ProcessDeletePage);
 //# sourceMappingURL=business.js.map
--- Server/Routes/business.ts
+++ Server/Routes/business.ts
@@ -5,20 +5,23 @@ export default router;
 // instantiate an object of type clothing controller
 import { DisplayAddPage, DisplayBusinessListPage, DisplayEditPage, ProcessAddPage, ProcessDeletePage, ProcessEditPage } from '../Controllers/business';
 
+// import Util functions
+import { AuthGuard } from '../Util/index';
+
 /* GET /clothing-list page. */
 router.get('/', DisplayBusinessListPage);
 
 /* GET - display /clothing-list/add page. */
-router.get('/add', DisplayAddPage);
+router.get('/add', AuthGuard, DisplayAddPage);
 
 /* GET - display /clothing-list/edit/:id page. */
-router.get('/edit/:id', DisplayEditPage);
+router.get('/edit/:id', AuthGuard, DisplayEditPage);
 
 /* POST - process /clothing-list/add page */
-router.post('/add', ProcessAddPage);
+router.post('/add', AuthGuard, ProcessAddPage);
 
 /* POST - process /clothing-list/edit/:id page */
-router.post('/edit/:id', ProcessEditPage);
+router.post('/edit/:id', AuthGuard, ProcessEditPage);
 
 /* GET - process /clothing-list/delete/:id */
-router.get('/delete/:id', ProcessDeletePage);
+router.get('/delete/:id', AuthGuard, ProcessDeletePage);
